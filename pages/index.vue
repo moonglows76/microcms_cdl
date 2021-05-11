516b4804fed90191c7ee6c0d8045c9db90aedcd1
森史憲 <mori@codingdesing.jp>
2021-05-11 11:01:05 +0900

    2nd commit

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul>
+    <li v-for="content in contents" :key="content.id">
+      <nuxt-link :to="`/${content.id}`">
+        {{ content.title }}
+      </nuxt-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  async asyncData() {
+    const { data } = await axios.get(
+      // your-service-id部分は自分のサービスidに置き換えてください
+      'https://cdl2021.microcms.io/api/v1/blog',
+      {
+        // your-api-key部分は自分のapi-keyに置き換えてください
+        headers: { 'X-API-KEY': 'a6bae4eb-c7cb-498f-b87c-426eb33c9f7b' }
+      }
+    )
+    return data
+  }
+}
+</script>
